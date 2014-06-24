224b3958deb8e34721f29879c2f37c93b420d10a
Tania Chattopadhyay <tchatterjee5@gmail.com>
2014-06-24 10:54:35 +0800

    Add files

---
+++ inter-mingle.py
@@ -0,0 +1,24 @@
+import cgi
+from google.appengine.api import users
+import webapp2
+import os
+
+
+f = open("page1.html","r")
+MAIN_PAGE_HTML = f.read()
+f.close()
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.write(MAIN_PAGE_HTML)
+
+class Guestbook(webapp2.RequestHandler):
+    def post(self):
+        self.response.write('<html><body>You wrote:<pre>')
+        self.response.write(cgi.escape(self.request.get('content')))
+        self.response.write('</pre></body></html>')
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/sign', Guestbook),
+], debug=True)
